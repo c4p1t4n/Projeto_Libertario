1642f37d92aa3c0f2dd817fa7461ec65c3e73198
c4p1t4n <leoigornunes@gmail.com>
2021-07-02 03:48:04 -0300

    Refatorando Models substituindo aspas simples por aspas duplas

--- Site-node/models/Autor.js
+++ Site-node/models/Autor.js
@@ -1,30 +1,30 @@
 module.exports = (sequelize, DataTypes) => {
-    let Autor = sequelize.define('Autor',{	
+    let Autor = sequelize.define("Autor",{	
 		idAutor: {
-			field: 'idAutor',
+			field: "idAutor",
 			type: DataTypes.INTEGER,
 			primaryKey: true,
 			autoIncrement: true
 		},	
 		nomeAutor: {
-			field: 'nomeAutor',
+			field: "nomeAutor",
 			type: DataTypes.STRING,
 			allowNull: false
 		},
 		nascimento: {
-			field: 'nascimento',
+			field: "nascimento",
 			type: DataTypes.DATEONLY,
 			allowNull: false
 		},
 		morte: {
-			field: 'morte',
+			field: "morte",
 			type: DataTypes.DATEONLY, // NÃO existe DATETIME. O tipo DATE aqui já tem data e hora
 			allowNull: false
 		},
 		
 	}, 
 	{
-		tableName: 'tbAutores', 
+		tableName: "tbAutores", 
 		freezeTableName: true, 
 		underscored: true,
 		timestamps: false,
