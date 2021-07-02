3c5edb31ca30193c6f70fedf379348e2ced9cb8e
c4p1t4n <leoigornunes@gmail.com>
2021-07-02 04:03:06 -0300

    Refatorando funcao cadastrar

--- Site-node/public/scripts/cadastrar.js
+++ Site-node/public/scripts/cadastrar.js
@@ -1,49 +1,82 @@
-function verificaoCadastrar(){
+function verificaoCadastrar() {
+    
+    
+    
+   
+};
+
+function verificarEmail() {
     var email = inputEmail.value;
-    console.log(email)
     var confirmacaoEmail = inputConfirmacaoEmail.value;
-    var senha = inputSenha.value;
-    var confirmacaoSenha = inputConfirmacaoSenha.value;
-    if (email != confirmacaoEmail){
-        if (email.indexOf("@") == -1 && email.indexOf(".com") == -1 || confirmacaoEmail.indexOf(".com") && confirmacaoEmailindexOf("@") ){
+    if (email != confirmacaoEmail) {
+        if (email.indexOf("@") == -1 && email.indexOf(".com") == -1 || confirmacaoEmail.indexOf(".com") && confirmacaoEmailindexOf("@")) {
             alert("Digite um email valido")
-            
-        }else{
-        alert("Emails não coincidem")
-        
+
+        } else {
+            alert("Emails não coincidem")
+
         }
-        
-    }
-    else if(senha != confirmacaoSenha){
-        alert("Senhas não coincidem")
-        
-       
+
     }
     else{
-        
+        verificarSenha()
+    }
+
+}
+function verificarSenha(){
+    var senha = inputSenha.value;
+    var confirmacaoSenha = inputConfirmacaoSenha.value;
+    if (senha !== confirmacaoSenha) {
+        alert("Senhas não coincidem")
+
+
+    }
+    else {
+
         cadastrar()
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function cadastrar() {
-    console.log
+
     var formulario = new URLSearchParams(new FormData(form_cadastro));
     fetch("/usuarios/cadastrar", {
         method: "POST",
         body: formulario
     }).then(function (response) {
-        
+
         if (response.ok) {
 
-            window.location.href='./login.html';
+            window.location.href = './login.html';
 
         } else {
-            
-            console.log('Erro de cadastro!');
+
+            ('Erro de cadastro!');
             response.text().then(function (resposta) {
                 div_erro.innerHTML = resposta;
             });
-            
-            
+
+
         }
     });
 
